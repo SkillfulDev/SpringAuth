ea21c75717e90ff51312bfd18239d16b778cdfd4
skillfuldev <chernonog.evgeniy@gmail.com>
2023-05-08 16:00:49 +0300

    add_trying1

--- SpringAuth/src/main/java/ua/chernonog/springauth/config/SecurityConfig.java
+++ SpringAuth/src/main/java/ua/chernonog/springauth/config/SecurityConfig.java
@@ -2,19 +2,19 @@ package ua.chernonog.springauth.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
-
     @Bean
-    public UserDetailsService userDetailsService() {
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        manager.createUser(User.withDefaultPasswordEncoder().username("user").password("password").roles().build());
-        return manager;
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
     }
 }
